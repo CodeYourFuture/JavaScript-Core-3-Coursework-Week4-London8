44839784351ae48e080bfb43c8cf70c7f644abe4
Margarita Glushkova <m.glushkova1@gmail.com>
2022-03-15 23:08:25 +0000

    Cleaner code for ner-roman.js

--- C-TDD/01-roman-numerals/convert-to-new-roman.js
+++ C-TDD/01-roman-numerals/convert-to-new-roman.js
@@ -4,19 +4,20 @@ function convertToNewRoman(n) {
     romanNumber += "M".repeat(Math.round(n / 1000) - 1);
   }
   let x = n % 1000;
+  let y = 900;
   romanNumber +=
-    "CM".repeat(x / 900) +
-    "D".repeat((x %= 900) / 500) +
-    "CD".repeat((x %= 500) / 400) +
-    "C".repeat((x %= 400) / 100) +
-    "XC".repeat((x %= 100) / 90) +
-    "L".repeat((x %= 90) / 50) +
-    "XL".repeat((x %= 50) / 40) +
-    "X".repeat((x %= 40) / 10) +
-    "IX".repeat((x %= 10) / 9) +
-    "V".repeat((x %= 9) / 5) +
-    "IV".repeat((x %= 5) / 4) +
-    "I".repeat((x %= 4));
+    "CM".repeat(x / y) +
+    "D".repeat((x %= y) / (y = 500)) +
+    "CD".repeat((x %= y) / (y = 400)) +
+    "C".repeat((x %= y) / (y = 100)) +
+    "XC".repeat((x %= y) / (y = 90)) +
+    "L".repeat((x %= y) / (y = 50)) +
+    "XL".repeat((x %= y) / (y = 40)) +
+    "X".repeat((x %= y) / (y = 10)) +
+    "IX".repeat((x %= y) / (y = 9)) +
+    "V".repeat((x %= y) / (y = 5)) +
+    "IV".repeat((x %= y) / (y = 4)) +
+    "I".repeat((x %= y));
   return romanNumber;
 }
 
