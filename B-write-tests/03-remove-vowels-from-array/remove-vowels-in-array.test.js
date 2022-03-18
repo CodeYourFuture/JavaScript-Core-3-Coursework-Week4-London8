c9fff2a77f4dd6a808a768a7ddf519fb5dc26fa1
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-03-18 13:29:41 +0000

    Update remove-vowels-in-array.test.js

--- B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -1,9 +1,10 @@
 let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
-  // Arrange
-  // Act
-  // Assert
+  const words = ["aeiuo", "kate", "coding", "notgood"];
+  const expectedAnswer = ["", "kt", "cdng", "ntgd"];
+  const receivedAnswer = removeVowelsFromWords(words);
+  expect(receivedAnswer).toEqual(expectedAnswer);
 });
 
 // example
