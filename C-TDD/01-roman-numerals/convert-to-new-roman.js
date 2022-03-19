621e6aac01d731ec8751bfb5f664a92f3d127654
Timea <timea.reich@gmail.com>
2022-03-19 17:24:50 +0000

    C-TDD 01-romain-numbers updated

--- C-TDD/01-roman-numerals/convert-to-new-roman.js
+++ C-TDD/01-roman-numerals/convert-to-new-roman.js
@@ -1,15 +1,27 @@
-function convertToNewRoman(n) {
-  let romanNumbers = {
-    1: "I",
-    5: "V",
-    10: "X",
-    50: "L",
-    100: "C",
-    500: "D",
-    100: "M",
+function convertToNewRoman(number) {
+  const romanNum = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
   };
-  return "";
+  let finalRomanNum = "";
+  for (key in romanNum) {
+    let n = Math.floor(number / romanNum[key]);
+    let remainingNum = number % romanNum[key];
+    finalRomanNum += key.repeat(n);
+    number = remainingNum;
+  }
+  return finalRomanNum;
 }
 
-console.log(9 % 5);
 module.exports = convertToNewRoman;
