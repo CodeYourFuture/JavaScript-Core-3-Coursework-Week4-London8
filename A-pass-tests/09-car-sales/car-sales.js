97fd4d82567a1f236494aefda1c3c5c2ff273d49
Harsheek Thanki <harsheekthanki@gmail.com>
2022-03-19 20:35:32 +0000

    some progress on car-sales.js

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,27 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  let allMakes = [];
+  for (let car in carsSold) {
+    allMakes.push(carsSold[car]["make"]);
+  }
+  let uniqueMakes = [...new Set(allMakes)].sort();
+  console.log(uniqueMakes);
+}
 
 module.exports = sales;
+
+//  let carsSold = [
+//    { make: "Ford", model: "Fiesta", colour: "Red", price: 5999 },
+//    { make: "Land Rover", model: "Defender", colour: "Muddy", price: 12000 },
+//    { make: "Toyota", model: "Prius", colour: "Silver", price: 6500 },
+//    { make: "Honda", model: "Civic", colour: "Yellow", price: 8000 },
+//    { make: "Ford", model: "Fiesta", colour: "Red", price: 15000 },
+//    { make: "Land Rover", model: "Discovery", colour: "Blue", price: 9000 },
+//    { make: "Ford", model: "Fiesta", colour: "Green", price: 2000 },
+//  ];
+
+//  let totals = {
+//    Ford: 22999,
+//    Honda: 8000,
+//    "Land Rover": 21000,
+//    Toyota: 6500,
+//  };
