485d2aa89d6cc5f3ce85a7f20d27f821cee7f907
mairacagri <mairacagri@gmail.com>
2022-03-21 14:34:50 +0000

    B-write test, cars-sale(a)
    cars-sale not finished

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,36 @@
-function sales(carsSold) {}
+//  expected result object
+//let totals = {
+//     Ford: 22999,
+//     Honda: 8000,
+//     "Land Rover": 21000,
+//     Toyota: 6500,
+//   };
+
+//let carsSold = [
+//     { make: "Ford", model: "Fiesta", colour: "Red", price: 5999 },
+//     { make: "Land Rover", model: "Defender", colour: "Muddy", price: 12000 },
+//     { make: "Toyota", model: "Prius", colour: "Silver", price: 6500 },
+//     { make: "Honda", model: "Civic", colour: "Yellow", price: 8000 },
+//     { make: "Ford", model: "Fiesta", colour: "Red", price: 15000 },
+//     { make: "Land Rover", model: "Discovery", colour: "Blue", price: 9000 },
+//     { make: "Ford", model: "Fiesta", colour: "Green", price: 2000 },
+//   ]; forEach(element)=>element.make and element.price
+
+//1 loop through carsSold array using .forEach() or
+//
+
+
+
+function sales(carsSold) {
+  carsSold.map((element) =>{
+     let {name, price} = element;//let name = element.make .... let price = element.price
+     if(name === Ford &&
+        name === Honda && 
+        name === LandRover&&
+        name === Toyota){
+            //do something
+        }
+  })
+}
 
 module.exports = sales;
--- B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -2,8 +2,12 @@ let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
   // Arrange
+  let words = ["Irina", "Etza", "Daniel"];
+  let expected = ["rn", "tz", "Dnl"];
   // Act
+  let result = removeVowelsFromWords(words);
   // Assert
+  expect(result).toEqual(expected);
 });
 
 // example
