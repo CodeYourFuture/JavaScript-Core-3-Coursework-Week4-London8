0bdd92b30605943c8b4832554057436517ffc8f8
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-03-21 12:55:38 +0000

    Add remove-vowels-test exercise

--- B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -2,8 +2,12 @@ let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
   // Arrange
+  const input = ["Irina", "Etza", "Daniel"];
+  const expected = ["rn", "tz", "Dnl"];
   // Act
+  const getVowels = removeVowelsFromWords(input);
   // Assert
+  expect(getVowels).toEqual(expected);
 });
 
 // example
