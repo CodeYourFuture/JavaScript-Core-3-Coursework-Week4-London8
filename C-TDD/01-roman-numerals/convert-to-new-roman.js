68da24790435cb3a66f59272ceefc6570dc969b0
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-03-21 16:42:48 +0000

    Add roman-numerals exercise

--- C-TDD/01-roman-numerals/convert-to-new-roman.js
+++ C-TDD/01-roman-numerals/convert-to-new-roman.js
@@ -1,4 +1,32 @@
 function convertToNewRoman(n) {
+const numerals = {
+  1: "I",
+  3: "III",
+  4: "IV",
+  5: "V",
+  9: "IX",
+  10: "X",
+  40: "XL",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M",
+};
+
+let romanized = "";
+const numericKeys = Object.keys(numerals).reverse();
+
+numericKeys.forEach((number) => {
+  while (number <= n) {
+    romanized += numerals[number];
+    n -= number;
+  }
+});
+
+return romanized;
 
 }
 
--- C-TDD/01-roman-numerals/convert-to-old-roman.js
+++ C-TDD/01-roman-numerals/convert-to-old-roman.js
@@ -1,5 +1,32 @@
-function convertToOldRoman(n) {
+const convertToOldRoman = (n) => {
 
+const numerals = {
+  1: "I",
+  3: "III",
+  5: "V",
+  9: "VIIII",
+  10: "X",
+  40: "XXXX",
+  50: "L",
+  90: "LXXXX",
+  100: "C",
+  400: "CCCC",
+  500: "D",
+  900: "DCCCC",
+  1000: "M"
+}
+
+let romanized = "";
+const numericKeys = Object.keys(numerals).reverse();
+
+numericKeys.forEach((number) => {
+  while(number <= n) {
+    romanized += numerals[number]
+    n -= number;
+  }
+})
+
+return romanized;
 }
 
 module.exports = convertToOldRoman;
