ee06fc36f63d92dd50767b88dd6401a3ed7316e4
Hamra <hamra.khan.b@gmail.com>
2022-03-22 15:22:04 +0000

    Completed B-write-tests

--- B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
+++ B-write-tests/03-remove-vowels-from-array/remove-vowels-in-array.test.js
@@ -2,8 +2,12 @@ let removeVowelsFromWords = require("./remove-vowels-in-array");
 
 test("remove vowels from all words in array", function () {
   // Arrange
+  const nameArray = ["Irina", "Etza", "Daniel"];
+  const expectedResult = ["rn", "tz", "Dnl"];
   // Act
+  const output = removeVowelsFromWords(nameArray);
   // Assert
+  expect(output).toEqual(expectedResult);
 });
 
 // example
