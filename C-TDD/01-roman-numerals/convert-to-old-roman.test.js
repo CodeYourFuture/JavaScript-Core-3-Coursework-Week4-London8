4f09e14558bfa9102e6f02a3c7407c82b64a2c4b
Hamra <hamra.khan.b@gmail.com>
2022-03-22 23:30:06 +0000

    Completed exercise covert-to-old-roman

--- C-TDD/01-roman-numerals/convert-to-old-roman.test.js
+++ C-TDD/01-roman-numerals/convert-to-old-roman.test.js
@@ -2,6 +2,10 @@ let convertToOldRoman = require("./convert-to-old-roman");
 
 test("returns I if passed 1 as an argument", function () {
   // Arrange
+  const n = 1;
+  const expectedResult = 'I';
   // Act
+  const output = convertToOldRoman(n);
   // Assert
+  expect(output).toEqual(expectedResult);
 });
