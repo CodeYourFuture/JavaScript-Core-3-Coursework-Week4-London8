09547b719670907e34230b78de920aa2e37f87a0
Hamra <hamra.khan.b@gmail.com>
2022-03-22 12:48:34 +0000

    Completed A-pass-tests

--- A-pass-tests/09-car-sales/car-sales.js
+++ A-pass-tests/09-car-sales/car-sales.js
@@ -1,3 +1,14 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+    let carMakeAndPrice = {};
+    for (let car of carsSold) {
+      if (carMakeAndPrice[car.make]) {
+        carMakeAndPrice[car.make] += car.price;
+      } else {
+        carMakeAndPrice[car.make] = car.price;
+      }
+    }
+    return carMakeAndPrice;
+        
+}
 
 module.exports = sales;
