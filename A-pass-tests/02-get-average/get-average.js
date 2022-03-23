e08ae9d1bb034a73c8764cab9dac25265974add7
Ika Khoza <ikaneng.khoza@gmail.com>
2022-03-23 10:13:26 +0000

    Completed 8 exercises
    completed the first few exercises on testing

--- A-pass-tests/02-get-average/get-average.js
+++ A-pass-tests/02-get-average/get-average.js
@@ -2,6 +2,11 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let result = numbers.filter((element) => typeof element === "number")
+    let average = result.reduce((previousValue, currentValue) => previousValue + currentValue,) / result.length
+    return average
+}
 
 module.exports = average;
+
