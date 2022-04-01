8c7714df92c313616fb1e51772736fbdee0d4a53
Hatice06 <haticeaydogan00@gmail.com>
2022-04-01 17:59:40 +0100

    TDD calculator exercise

---
+++ C-TDD/03-calculator/calculator.js
@@ -0,0 +1,16 @@
+
+function add(numbers) {
+  if(numbers === ""){
+    return 0;
+  }
+  const numbersArr =numbers.split(",");
+  if(numbers.split(",")){
+    return Number(numbersArr[0]) ;
+  }
+
+}
+
+
+
+
+module.exports= add;
---
+++ C-TDD/03-calculator/calculator.test.js
@@ -0,0 +1,19 @@
+let add = require("./calculator");
+
+test("returns 0 if passed an empty string", function(){
+  const emptyString ="";
+  const expectedResult = 0;
+
+  const output = add(emptyString);
+
+  expect(output).toEqual(expectedResult);
+})
+
+test("returns 1 if passed '1'", function () {
+  const stringNum = "1";
+  const expectedResult = 1;
+
+  const output = add(stringNum);
+
+  expect(output).toEqual(expectedResult);
+});
